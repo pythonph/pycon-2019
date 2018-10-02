90acc7875f92e5570b01033633e98c74d3094ee3
Jomari Macaraig <macaraig.jomari10@gmail.com>
2018-10-02 08:56:37 +0000

    Created proposals app

--- config/settings/base.py
+++ config/settings/base.py
@@ -71,6 +71,7 @@ THIRD_PARTY_APPS = [
 ]
 LOCAL_APPS = [
     'pycon.users.apps.UsersAppConfig',
+    'pycon.proposals.apps.ProposalsConfig',
     # Your stuff: custom apps go here
 ]
 # https://docs.djangoproject.com/en/dev/ref/settings/#installed-apps
