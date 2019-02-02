3824c8f1f3f8002690e3b2d78d76d328600c0dbd
Mitchel Cabuloy <mixxorz@gmail.com>
2019-02-02 16:58:37 +0800

    Add CSS filters to speaker photos

---
+++ pycon/core/templatetags/core_tags.py
@@ -0,0 +1,26 @@
+import random
+
+from django import template
+
+register = template.Library()
+
+
+@register.simple_tag
+def blend_class(seed=None, *exclude):
+    if seed:
+        random.seed(int(seed))
+    exclude = set(exclude)
+    all_colors = {
+        'red',
+        'orange',
+        'blue',
+        'yellow',
+        'purple',
+        'green',
+        'pink',
+        'blue-yellow',
+        'pink-yellow',
+        # 'red-blue',
+    }
+    colors = list(all_colors - exclude)
+    return f'blend-{random.choice(colors)}'
