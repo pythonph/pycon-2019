8b24ce2d608e47bbc017d472929f45e33ae6146d
Mitchel Cabuloy <mixxorz@gmail.com>
2019-02-02 16:08:52 +0800

    Add sponsor tooltips

--- pycon/static/scripts/main.js
+++ pycon/static/scripts/main.js
@@ -1,5 +1,22 @@
 import 'babel-polyfill'
 import Parallax from 'parallax-js'
+import tippy from 'tippy.js'
 
 new Parallax(document.querySelector('#scene_1'));
 new Parallax(document.querySelector('#scene_2'));
+
+document.querySelectorAll('.sponsors-list__sponsor').forEach(el => {
+  const img = el.querySelector('img');
+  const description = el.querySelector('.tooltip-box__wrapper');
+
+  tippy(img, {
+    animateFill: false,
+    animation: 'fade',
+    arrow: true,
+    content: description,
+    interactive: true,
+    performance: true,
+    placement: 'top',
+    size: 'large',
+  })
+})
